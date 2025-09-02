a7d2b8373137a28185591bfdbfbec1788bf3bc24
hanZ3tton <zidane.kr@icloud.com>
2025-09-02 16:10:36 +0700

    adding .gitignore

--- application/views/layouts/admin/sidebar.php
+++ application/views/layouts/admin/sidebar.php
@@ -1399,6 +1399,96 @@
                      </div>
                      <!--end:Menu item-->
                      <!--begin:Menu item-->
+                     <div class="menu-item pt-5">
+                         <!--begin:Menu content-->
+                         <div class="menu-content">
+                             <span class="menu-heading fw-bold text-uppercase fs-7">Settings</span>
+                         </div>
+                         <!--end:Menu content-->
+                     </div>
+                     <!--end:Menu item-->
+                     <!--begin:Menu item-->
+                     <div
+                         data-kt-menu-trigger="click"
+                         class="menu-item menu-accordion">
+                         <!--begin:Menu link-->
+                         <span class="menu-link">
+                             <span class="menu-icon">
+                                 <i class="ki-duotone ki-abstract-28 fs-2">
+                                     <span class="path1"></span>
+                                     <span class="path2"></span>
+                                     <span class="path3"></span>
+                                 </i>
+                             </span>
+                             <span class="menu-title">User Management</span>
+                             <span class="menu-arrow"></span>
+                         </span>
+                         <!--end:Menu link-->
+                         <!--begin:Menu sub-->
+                         <div class="menu-sub menu-sub-accordion">
+                             <!--begin:Menu item-->
+                             <div class="menu-item">
+                                 <!--begin:Menu link-->
+                                 <a
+                                     class="menu-link"
+                                     href="pages/user-profile/overview.html">
+                                     <span class="menu-bullet">
+                                         <span class="bullet bullet-dot"></span>
+                                     </span>
+                                     <span class="menu-title">All User</span>
+                                 </a>
+                                 <!--end:Menu link-->
+                             </div>
+                             <!--end:Menu item-->
+                             <!--begin:Menu item-->
+                             <div class="menu-item">
+                                 <!--begin:Menu link-->
+                                 <a
+                                     class="menu-link"
+                                     href="pages/user-profile/projects.html">
+                                     <span class="menu-bullet">
+                                         <span class="bullet bullet-dot"></span>
+                                     </span>
+                                     <span class="menu-title">Courier</span>
+                                 </a>
+                                 <!--end:Menu link-->
+                             </div>
+                             <!--end:Menu item-->
+                             <!--begin:Menu item-->
+                             <div class="menu-item">
+                                 <!--begin:Menu link-->
+                                 <a
+                                     class="menu-link"
+                                     href="pages/user-profile/campaigns.html">
+                                     <span class="menu-bullet">
+                                         <span class="bullet bullet-dot"></span>
+                                     </span>
+                                     <span class="menu-title">Staff</span>
+                                 </a>
+                                 <!--end:Menu link-->
+                             </div>
+                             <!--end:Menu item-->
+                             <!--begin:Menu item-->
+                             <div class="menu-item">
+                                 <!--begin:Menu link-->
+                                 <a
+                                     class="menu-link"
+                                     href="pages/user-profile/documents.html">
+                                     <span class="menu-bullet">
+                                         <span class="bullet bullet-dot"></span>
+                                     </span>
+                                     <span class="menu-title">Customers</span>
+                                 </a>
+                                 <!--end:Menu link-->
+                             </div>
+                             <!--end:Menu item-->
+
+
+                         </div>
+                         <!--end:Menu sub-->
+                     </div>
+                     <!--end:Menu item-->
+                     <!--begin:Menu item-->
                      <div
                          data-kt-menu-trigger="click"
                          class="menu-item menu-accordion">
