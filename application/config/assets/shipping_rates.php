6c6f30795056df0d493b5bd6dd4e7d0c55e54e83
PyThon4Zzz <radjabadii9@gmail.com>
2025-09-03 19:09:10 +0700

    shipping rates

---
+++ application/config/assets/shipping_rates.php
@@ -0,0 +1,19 @@
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['assets'] = [
+    'js' => [
+        "assets/plugins/custom/datatables/datatables.bundle.js",
+"assets/js/custom/apps/invoices/create.js",
+"assets/js/widgets.bundle.js",
+"assets/js/custom/widgets.js",
+"assets/js/custom/apps/chat/chat.js",
+"assets/js/custom/utilities/modals/upgrade-plan.js",
+"assets/js/custom/utilities/modals/create-app.js",
+"assets/js/custom/utilities/modals/users-search.js",
+    ],
+    'css' => [
+        'assets/plugins/custom/datatables/datatables.bundle.css'
+    ]
+];
---
+++ application/controllers/admin/Shipping_rates.php
@@ -0,0 +1,25 @@
+    <?php
+    defined('BASEPATH') or exit('No direct script access allowed');
+
+    class Shipping_rates extends MY_Controller
+    {
+
+        public function __construct()
+        {
+            parent::__construct();
+
+            $this->defaultLayout = 'layouts/app';
+        }
+
+        public function index()
+        {
+            $data = [];
+
+            $this->config->load('assets/shipping_rates');
+            $page_assets = $this->config->item('assets');
+            $this->pageScripts =  $page_assets['js'];
+            $this->pageStyles =  $page_assets['css'];
+
+            $this->loadView('admin/shipping_rates/index', 'Shipping Rates', $data);
+        }
+    }
