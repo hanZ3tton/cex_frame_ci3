ef87dd3070d00c83ea9e2c80d133ba9473a75d14
hanZ3tton <zidane.kr@icloud.com>
2025-09-04 15:54:45 +0700

    Adding database

---
+++ application/database/04_09_2025_1548_create_table_user.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `users`(
+    `id` INT NOT NULL,
+    `role_id` INT NOT NULL DEFAULT 1,
+    `username` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(20) NULL,
+    `agent` VARCHAR(128) NULL,
+    `address` TEXT,
+    `profile_picture` VARCHAR(255),
+    `is_active` TINYINT NOT NULL,
+    `last_login` DATETIME NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NULL,
+    `deleted_at` DATETIME NULL
+);  
