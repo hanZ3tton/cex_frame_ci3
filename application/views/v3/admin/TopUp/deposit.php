5265ff6cca356c01435243d9b3d5ed9be3a205e5
PyThon4Zzz <radjabadii9@gmail.com>
2025-09-09 11:40:46 +0700

    added deposit form

--- application/views/v3/admin/TopUp/deposit.php
+++ application/views/v3/admin/TopUp/deposit.php
@@ -6,7 +6,7 @@
 									<!--begin::Page title-->
 									<div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
 										<!--begin::Title-->
-										<h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">Download TopUp</h1>
+										<h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">Topup Deposit</h1>
 										<!--end::Title-->
 										<!--begin::Breadcrumb-->
 										<ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
@@ -52,22 +52,20 @@
 									<!--begin::Billing Address-->
 									<!--end::Billing Address-->
 									<!--begin::Billing History-->
-									<div class="d-flex flex-center">
-                                        <div class="card card-flush h-lg-100" id="kt_contacts_main">
+									<!--begin::Wrapper-->
+									<div class="d-flex flex-column flex-center flex-column-fluid">
+										<!--begin::Card-->
+										<div class="card card-flush h-lg-100 w-md-500px mx-auto" id="kt_contacts_main">
 											<!--begin::Card header-->
 											<div class="card-header pt-3" id="kt_chat_contacts_header">
 												<!--begin::Card title-->
 												<div class="card-title">
-													<i class="ki-duotone ki-badge fs-1 me-2">
-														<span class="path1"></span>
-														<span class="path2"></span>
-														<span class="path3"></span>
-														<span class="path4"></span>
-														<span class="path5"></span>
-													</i>
-													<h2>User Form</h2>
+													<h2>Add Deposit</h2>
 												</div>
 												<!--end::Card title-->
+												<!--begin::Close button-->
+												
+												<!--end::Close button-->
 											</div>
 											<!--end::Card header-->
 											<!--begin::Card body-->
@@ -75,11 +73,94 @@
 												<!--begin::Form-->
 												<form id="kt_ecommerce_settings_general_form" class="form" action="#">
 													<!--begin::Input group-->
-													<div class="mb-7">
+													<div class="fv-row mb-7">
 														<!--begin::Label-->
-														<label class="fs-6 fw-semibold mb-3">
-															<span>Avatar</span>
-															<span class="ms-1" data-bs-toggle="tooltip" title="Allowed file types: png, jpg, jpeg.">
+														<label class="fs-6 fw-semibold form-label mt-3">
+															<span class="required">Bank Name</span>
+															<span class="ms-1" data-bs-toggle="tooltip" title="Enter The Bank Name">
+																<i class="ki-duotone ki-information fs-7">
+																	<span class="path1"></span>
+																	<span class="path2"></span>
+																	<span class="path3"></span>
+																</i>
+															</span>
+														</label>
+														<!--end::Label-->
+														<!--begin::Input-->
+														<input type="text" placeholder="Bank Name" class="form-control form-control-solid" name="BankName" value="" />
+														<!--end::Input-->
+													</div>
+													<!--end::Input group-->
+
+													<!--begin::Input group-->
+													<div class="fv-row mb-7">
+														<!--begin::Label-->
+														<label class="fs-6 fw-semibold form-label mt-3">
+															<span class="required">Bank Account Name</span>
+															<span class="ms-1" data-bs-toggle="tooltip" title="Enter The Bank Account Name">
+																<i class="ki-duotone ki-information fs-7">
+																	<span class="path1"></span>
+																	<span class="path2"></span>
+																	<span class="path3"></span>
+																</i>
+															</span>
+														</label>
+														<!--end::Label-->
+														<!--begin::Input-->
+														<input type="text" placeholder="Bank Account Name" class="form-control form-control-solid" name="BankAccountName" value="" />
+														<!--end::Input-->
+													</div>
+													<!--end::Input group-->
+
+													<!--begin::Input group-->
+													<div class="fv-row mb-7">
+														<!--begin::Label-->
+														<label class="fs-6 fw-semibold form-label mt-3">
+															<span class="required">Bank Destination</span>
+															<span class="ms-1" data-bs-toggle="tooltip" title="Enter The Bank Destination">
+																<i class="ki-duotone ki-information fs-7">
+																	<span class="path1"></span>
+																	<span class="path2"></span>
+																	<span class="path3"></span>
+																</i>
+															</span>
+														</label>
+														<!--end::Label-->
+														<!--begin::Select-->
+														<select class="form-select form-control-solid" data-control="select2" name="BankDestination" data-placeholder="Select a bank destination">
+															<option value="BCA 586-520-2641">BCA 586-520-2641</option>
+															<!-- Other options can be added here -->
+														</select>
+														<!--end::Select-->
+													</div>
+													<!--end::Input group-->
+
+													<!--begin::Input group-->
+													<div class="fv-row mb-7">
+														<!--begin::Label-->
+														<label class="fs-6 fw-semibold form-label mt-3">
+															<span class="required">Nominal</span>
+															<span class="ms-1" data-bs-toggle="tooltip" title="Enter the deposit amount">
+																<i class="ki-duotone ki-information fs-7">
+																	<span class="path1"></span>
+																	<span class="path2"></span>
+																	<span class="path3"></span>
+																</i>
+															</span>
+														</label>
+														<!--end::Label-->
+														<!--begin::Input-->
+														<input type="text" placeholder="Nominal..." class="form-control form-control-solid" name="Nominal" value="" />
+														<!--end::Input-->
+													</div>
+													<!--end::Input group-->
+													
+													<!--begin::Input group for Transfer Proof-->
+													<div class="fv-row mb-7">
+														<!--begin::Label-->
+														<label class="fs-6 fw-semibold form-label mt-3">
+															<span class="required">Transfer Proof</span>
+															<span class="ms-1" data-bs-toggle="tooltip" title="Upload a screenshot of the transfer proof">
 																<i class="ki-duotone ki-information fs-7">
 																	<span class="path1"></span>
 																	<span class="path2"></span>
@@ -95,14 +176,13 @@
 																.image-input-placeholder {
 																	background-image: url('assets/media/svg/files/blank-image.svg');
 																}
-
 																[data-bs-theme="dark"] .image-input-placeholder {
 																	background-image: url('assets/media/svg/files/blank-image-dark.svg');
 																}
 															</style>
 															<!--end::Image placeholder-->
 															<!--begin::Image input-->
-															<div class="image-input image-input-outline image-input-placeholder image-input-empty image-input-empty" data-kt-image-input="true">
+															<div class="image-input image-input-outline image-input-placeholder image-input-empty" data-kt-image-input="true">
 																<!--begin::Preview existing avatar-->
 																<div class="image-input-wrapper w-100px h-100px" style="background-image: url('')"></div>
 																<!--end::Preview existing avatar-->
@@ -139,107 +219,21 @@
 														</div>
 														<!--end::Image input wrapper-->
 													</div>
-													<!--end::Input group-->
-													<!--begin::Input group-->
-													<div class="fv-row mb-7">
-														<!--begin::Label-->
-														<label class="fs-6 fw-semibold form-label mt-3">
-															<span class="required">Full Name</span>
-															<span class="ms-1" data-bs-toggle="tooltip" title="Enter the contact's name.">
-																<i class="ki-duotone ki-information fs-7">
-																	<span class="path1"></span>
-																	<span class="path2"></span>
-																	<span class="path3"></span>
-																</i>
-															</span>
-														</label>
-														<!--end::Label-->
-														<!--begin::Input-->
-														<input type="text" class="form-control form-control-solid" name="name" value="" />
-														<!--end::Input-->
+													<!--end::Input group for Transfer Proof-->	
+													<!--begin::Action buttons-->
+													<div class="d-flex justify-content-end pt-5">
+														<button type="reset" class="btn btn-light me-3" data-kt-contacts-type="cancel">Cancel</button>
+														<button type="submit" class="btn btn-primary">Save</button>
 													</div>
-													<!--end::Input group-->
-
-													<!--begin::Input group-->
-													<div class="fv-row mb-7">
-														<!--begin::Label-->
-														<label class="fs-6 fw-semibold form-label mt-3">
-															<span class="required">Email</span>
-															<span class="ms-1" data-bs-toggle="tooltip" title="Enter the contact's company name (optional).">
-																<i class="ki-duotone ki-information fs-7">
-																	<span class="path1"></span>
-																	<span class="path2"></span>
-																	<span class="path3"></span>
-																</i>
-															</span>
-														</label>
-														<!--end::Label-->
-														<!--begin::Input-->
-														<input type="text" class="form-control form-control-solid" name="company_name" value="" />
-														<!--end::Input-->
-													</div>
-													<!--end::Input group-->
-
-													<!--begin::Input group-->
-													<div class="fv-row mb-7">
-														<!--begin::Label-->
-														<label class="fs-6 fw-semibold form-label mt-3">
-															<span class="required">Password</span>
-															<span class="ms-1" data-bs-toggle="tooltip" title="Enter the contact's company name (optional).">
-																<i class="ki-duotone ki-information fs-7">
-																	<span class="path1"></span>
-																	<span class="path2"></span>
-																	<span class="path3"></span>
-																</i>
-															</span>
-														</label>
-														<!--end::Label-->
-														<!--begin::Input-->
-														<input type="password" class="form-control form-control-solid" name="company_name" value="" />
-														<!--end::Input-->
-													</div>
-													<!--end::Input group-->
-
-													<!--begin::Input group-->
-													<div class="fv-row mb-7">
-														<!--begin::Label-->
-														<label class="fs-6 fw-semibold form-label mt-3">
-															<span class="required">Confrim Password</span>
-															<span class="ms-1" data-bs-toggle="tooltip" title="Enter the contact's company name (optional).">
-																<i class="ki-duotone ki-information fs-7">
-																	<span class="path1"></span>
-																	<span class="path2"></span>
-																	<span class="path3"></span>
-																</i>
-															</span>
-														</label>
-														<!--end::Label-->
-														<!--begin::Input-->
-														<input type="text" class="form-control form-control-solid" name="company_name" value="" />
-														<!--end::Input-->
-													</div>
-													<!--end::Input group-->
-
-													<!--begin::Input group-->
-													
-													<!--end::Input group-->
-
-
-													<!--begin::Roles-->
-													<!--begin::Input row-->
-													
-													<!--end::Input row-->
-
-												
-													<!--begin::Input row-->
-													
 													<!--end::Action buttons-->
 												</form>
 												<!--end::Form-->
 											</div>
 											<!--end::Card body-->
 										</div>
-                                    </div>  
+										<!--end::Card-->
+									</div>
+									<!--end::Wrapper-->		
 									<!--end::Billing Address-->
 								</div>
 								<!--end::Content container-->
